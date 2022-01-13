23eee22fd822ac2ca604aa7f5cc52da07a98a116
Emeraude248 <96231220+Emeraude248@users.noreply.github.com>
2022-01-13 11:44:19 +0100

    this_bind

--- le soucis du this.js
+++ le soucis du this.js
@@ -53,3 +53,50 @@ handleClick() {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+//pas de soucis de this ici, la fonction fléchée ne perd pas le contexte
+
+<button onClick={() => this.handleClick()}>Click me</button>
+
+//autre posibilité, handleCick comme propriété de la class App et appel d'une focntion fléchée, mais codesandbox n'a pas l'air d'aimer ^^
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+class App extends React.Component {
+
+state = {
+  clients: [
+    {id: 1, nom: "Lior Chamla"},
+    {id: 2, nom: "Magali Perrin"},
+    {id: 3, nom: "Joseph Durand"}
+  ]
+}
+
+handleClick = () => {
+  console.log(this.state);
+}
+
+  render() {
+    const title = "Liste des clients";
+
+    return <div>
+      <h1>{title}</h1>
+      <button onClick={this.handleClick}>Click me</button>
+      <ul>
+        {this.state.clients.map((client) =>
+        <li>{client.nom} <button> X </button></li>)}
+      </ul>
+      <form>
+        <input type="text" placeholder="ajouter un client"/>
+          <button>Confirmer
+          </button>
+      </form>
+    </div>
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
